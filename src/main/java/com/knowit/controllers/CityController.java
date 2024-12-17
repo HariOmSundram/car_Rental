752ccaf938dad10bc73357636ff7dad2bd003554
VICKY <goutamgdy@gmail.com>
2024-12-18 00:18:02 +0530

    GCommit

--- src/main/java/com/knowit/controllers/CityController.java
+++ src/main/java/com/knowit/controllers/CityController.java
@@ -14,7 +14,7 @@ public class CityController {
 	@Autowired
 	CityService cser;
 	
-	@GetMapping("/getallcitie")
+	@GetMapping("/getallcities")
 	public List<City> getAllCITIES() {
 		return cser.getallCity();
 	}
