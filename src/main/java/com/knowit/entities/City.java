930ea649fe9dfe45dde96709448e08d641e634b7
HariOmSundram <hariomr615@gmail.com>
2024-12-17 23:20:57 +0530

    commit changes

--- src/main/java/com/knowit/entities/City.java
+++ src/main/java/com/knowit/entities/City.java
@@ -1,5 +1,6 @@
 package com.knowit.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,5 +20,6 @@ public class City {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 int cityid;
+@Column
 String cityname;
 }
