e0a380acad4d3f7dfc111dd45b4c2b9f8e2083fa
goutamgdy <91448655+goutamgdy@users.noreply.github.com>
2024-12-22 00:00:34 +0530

    Overlap
    Corrected

--- src/main/java/com/knowit/entities/Category.java
+++ src/main/java/com/knowit/entities/Category.java
@@ -1,9 +1,16 @@
 package com.knowit.entities;
 
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,4 +30,9 @@ public class Category {
     int categoryid;
     String category_name;
     String description;
+
+     @JsonIgnoreProperties("categoryid")
+    @OneToMany(mappedBy ="categoryid",cascade = CascadeType.ALL )
+    Set<Car> cars;
+
 }
--- src/main/java/com/knowit/entities/Fuel.java
+++ src/main/java/com/knowit/entities/Fuel.java
@@ -1,9 +1,18 @@
 package com.knowit.entities;
 
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,4 +30,8 @@ public class Fuel {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 int fuelid;
 String fueltype;
+ @JsonIgnoreProperties("fuelid")
+    @OneToMany(mappedBy ="fuelid",cascade = CascadeType.ALL )
+    Set<CarModel> carModels;
+
 }
