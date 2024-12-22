9f6ade436c9ddc8c33383dd9c977c69e8fb4fdbe
Hari Om Sundram <75016666+HariOmSundram@users.noreply.github.com>
2024-12-22 17:37:14 +0530

    @getters were missing
    corrected it

--- src/main/java/com/knowit/entities/Company.java
+++ src/main/java/com/knowit/entities/Company.java
@@ -5,13 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
+@Getter
 @Entity
-@Table(name = "company_of_car")
+@Table(name = "company_of _car")
 public class Company {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int idcompany;
+    int modelid;
 	String companyname;
 
 }
