f1dce50f54b9a80708a850e5c8ba8d7c5853b9e3
Hari Om Sundram <75016666+HariOmSundram@users.noreply.github.com>
2024-12-23 17:05:54 +0530

    Update UserController.java

--- src/main/java/com/knowit/controllers/UserController.java
+++ src/main/java/com/knowit/controllers/UserController.java
@@ -1,21 +1,13 @@
 package com.knowit.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.knowit.entities.User;
 import com.knowit.services.UserServices;
 import org.springframework.web.bind.annotation.PostMapping;
-
-
-
 import org.springframework.web.bind.annotation.RequestBody;
-
-
-
 import org.springframework.web.bind.annotation.RequestParam;
 
 
