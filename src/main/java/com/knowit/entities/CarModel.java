bf58740d77c6c33b704d94fb3140a9c611046bb6
iamherehaha12 <aatif3321@gmail.com>
2024-12-23 14:50:33 +0530

    changes
    car and carmodel associtaion

--- src/main/java/com/knowit/entities/CarModel.java
+++ src/main/java/com/knowit/entities/CarModel.java
@@ -15,6 +15,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 
--- src/main/java/com/knowit/entities/Fuel.java
+++ src/main/java/com/knowit/entities/Fuel.java
@@ -5,10 +5,16 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.CascadeType;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
