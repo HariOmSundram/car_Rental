07cdfe4672e84f92c85fcd76d5696a1565ff65be
Hari Om Sundram <75016666+HariOmSundram@users.noreply.github.com>
2024-12-25 20:59:02 +0530

    Filter Module on Cars

--- src/main/java/com/knowit/entities/City.java
+++ src/main/java/com/knowit/entities/City.java
@@ -1,20 +1,21 @@
 package com.knowit.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-@Getter
-@NoArgsConstructor
-@Setter
-@AllArgsConstructor
+@Data
 @Entity
 @Table(name="city")
+@JsonIgnoreProperties("users")
 public class City {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
